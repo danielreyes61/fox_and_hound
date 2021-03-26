32fac836e9286ce5f1f312bbec24d5956e060c93
Daniel Leo Reyes <danielreyes61@gmail.com>
2021-03-25 18:05:27 -0700

    Working on getting addresses

--- fox_and_hound.py
+++ fox_and_hound.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 import time
-
+from flask import Flask, redirect, url_for, render_template
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging']) # used to suppress random Bluetooth usb device warnings passed through chrome while using selenium
@@ -14,11 +14,37 @@ options.headless = True
 DRIVER_PATH = './chromedriver'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=options)
 driver.get('https://www.watkinsrealestateteam.com/listings-search/')
-#time.sleep(3)
+
 
 html = driver.page_source
 
 soup = BeautifulSoup(html,'lxml')
-title = soup.select('span.pl_listing-mlsId')
-mls_nums = [item.text for item in title]
-print(mls_nums)
+
+
+
+
+mls_id = soup.select('span.pl_listing-mlsId')
+mls_nums = [item.text for item in mls_id]
+
+price = soup.select('span.pl_listing-price')
+price_list = [item.text for item in price]
+
+
+#address = soup.select(['a']['class'])   ----------------- Address not working
+
+#print(address)
+# price_list = [item.text for item in price]
+
+
+
+
+
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template("index.html", mls=mls_nums, price=price_list)
+
+
+if __name__ == '__main__':
+    app.run()
